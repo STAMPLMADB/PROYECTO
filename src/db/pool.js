952038d6d2c0699daca832d98029bc192bb38c5a
LuisManuel Dominguez <lmadb1991@gmail.com>
2023-12-08 21:05:16 +0100

    base de datos sirve

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,13 +1,12 @@
 import "dotenv/config";
 import mysql from "mysql2/promise";
 
-const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD} = process.env;
 
 export const pool = mysql.createPool({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
     port: DB_PORT,
-    database: DB_NAME
 });
   
