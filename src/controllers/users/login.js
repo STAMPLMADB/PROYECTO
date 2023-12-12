0e5494b0b1050b5fbff4519b293d4b010562358e
Miguel Garcia <migueldariogq.mdg@gmail.com>
2023-12-12 21:36:29 +0100

    moreissues

--- src/controllers/users/login.js
+++ src/controllers/users/login.js
@@ -1,37 +1,39 @@
 import bcrypt from "bcrypt";
-// import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import selectUserByEmail from "../../models/users/selectUserByEmail.js";
-// import generateError from "../../utils/generateError.js";
+import generateError from "../../utils/generateError.js";
+
 
 const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    console.log(password);
 
     const userDb = await selectUserByEmail(email);
-    console.log(userDb.password);
 
     if (!userDb) {
-      res.send("El email  son incorrectos", 400);
+      generateError("El email es incorrecto",400);
     }
+
     const isPasswordOk = await bcrypt.compare(password, userDb.password);
 
     if (!isPasswordOk) {
-      res.send("El la contraseña son incorrectos", 400);
+      generateError("La contraseña es incorrecta",400);
     }
 
-    if (userDb.isEmailValidated === false) {
-      res.send("Esta cuenta no ha sido verificada, revisa tu email", 400);
+    // Revisar cómo se obtiene userDb.isEmailValidated de la base de datos
+
+    if (userDb.isEmailValidated === 0) {
+      generateError("La cuenta no ha sido verificada, revisa tu email",400);
     }
+
+    // Si todos los controles son exitosos, puedes generar el token JWT aquí
 
     const jwtPayload = { id: userDb.id };
+    const token = jwt.sign(jwtPayload, process.env.TOKEN_SECRET, {
+      expiresIn: "7d",
+    });
 
-    // const token = jwt.sign(jwtPayload, process.env.TOKEN_SECRET, {
-    //   expiresIn: "7d",
-    // });
-
-    res.send({ message: "Login correcto" });
-    console.log("todo en orden");
+    res.send({ message: "Loggeado correctamente", data: { token } });
   } catch (error) {
     next(error);
   }
