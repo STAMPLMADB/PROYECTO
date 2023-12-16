eb2c75350395ff94d65214318d128740a5b1787b
marta <105271643+ffanecaa@users.noreply.github.com>
2023-12-16 17:30:23 +0100

    sin review

--- src/controllers/reservation/controllerReservation.js
+++ src/controllers/reservation/controllerReservation.js
@@ -6,16 +6,16 @@ import { v4 as uuidv4 } from "uuid";
 const controllerReservation = async (req, res, next) => {
   try {
     const productId = req.query.productId; // Obtener productId de los parámetros de la consulta
-    const { reservationLocation, status, review } = req.body;
+    const { reservationLocation, status } = req.body;
     const reservationDate = new Date().toISOString().slice(0, 19).replace("T", " ");
 
     const schema = Joi.object().keys({
       reservationLocation: Joi.string().required(),
       status: Joi.string().valid("pendiente", "en proceso", "finalizada").required(),
-      review: Joi.string().valid("1", "2", "3", "4", "5").required(),
+    //  review: Joi.string().valid("1", "2", "3", "4", "5").required(),
     });
 
-    const validation = schema.validate({ reservationLocation, status, review });
+    const validation = schema.validate({ reservationLocation, status });
 
     if (validation.error) {
       return res.status(400).json({ error: validation.error.message });
@@ -28,7 +28,7 @@ const controllerReservation = async (req, res, next) => {
         reservationLocation,
         reservationDate,
         status,
-        review,
+     
       },
       buyerId,
       productId // Pasar productId como parámetro
