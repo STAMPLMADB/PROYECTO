35b9870d0e0f218024182eb7e5b714dd69cec663
git config --global user.name <dani.vazquez.gonzalez@gmail.com>
2024-01-23 17:14:20 +0100

    Corrección Db

--- src/controllers/products/controllerCreateProductId.js
+++ src/controllers/products/controllerCreateProductId.js
@@ -25,8 +25,9 @@ const controllerCreateProductId = async (req, res, next) => {
       "consola",
       "ordenador",
       "radio",
-      "televisor",
+      "videjuegos",
       "movil",
+      "otros",
     ];
     const schema = Joi.object().keys({
       name: Joi.string().min(1).max(80).required(),
