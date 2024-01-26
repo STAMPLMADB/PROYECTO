505f84e9c06ec09fe8f47c8deb8a56643c6bd9a1
LuisManuel Dominguez <lmadb1991@gmail.com>
2024-01-26 17:51:30 +0100

    controllerGetReservations extrae mas datos

--- src/controllers/reservation/controllerGetReservations.js
+++ src/controllers/reservation/controllerGetReservations.js
@@ -1,5 +1,5 @@
 import pool from "../../db/pool.js";
-
+// controlado con el que extraigo todos los datos necesarios para mostrar una reserva en este caso vemos en el SQL que me traigo todo de las tablas products, reservations y me traigo el nombre, email e imagen de perfil del comprador por ultimo estos datos se traen en base al id del comprador
 const controllerGetReservations = async (req, res, next) => {
   try {
     const loggedUserId = req.user.id;
