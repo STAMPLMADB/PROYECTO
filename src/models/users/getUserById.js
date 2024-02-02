8ca5f4028d6039238a8a03f6363c660465519682
git config --global user.name <dani.vazquez.gonzalez@gmail.com>
2024-02-02 20:10:28 +0100

    arreglo user

--- src/models/users/getUserById.js
+++ src/models/users/getUserById.js
@@ -3,8 +3,8 @@ import pool from "../../db/pool.js";
 const GetUserById = async (userId) => {
   
   const query = "SELECT * FROM users WHERE users.id = ?";
-  const [rows] = await pool.query(query, [userId]);
-  return rows;
+  const [rows] = await pool.query(query, userId);
+  return rows[0];
 };
 
 export default GetUserById;
