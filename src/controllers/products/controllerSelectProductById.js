500185133523d6aea24c6abf8ea5e8148863ddd3
Miguel Garcia <migueldariogq.mdg@gmail.com>
2024-02-05 03:19:07 +0100

    some fixes

--- src/controllers/products/controllerSelectProductById.js
+++ src/controllers/products/controllerSelectProductById.js
@@ -7,12 +7,12 @@ const controllerSelectProductById = async (req, res, next) => {
         // por si no hay un productId
         return res.status(400).json({ error: "se necesita un ID de producto" });
       }
-    const product = await selectProductById(productId);
-    if (!product) {
+    const data = await selectProductById(productId);
+    if (!data) {
         // por si el productId no existe o no se ha creado
         return res.status(404).json({ error: "Producto no Encontrado" });
       }
-    res.status(200).json(product);
+    res.status(200).json(data);
   } catch (error) {
     next(error);
   }
--- src/models/products/selectProductById.js
+++ src/models/products/selectProductById.js
@@ -5,7 +5,15 @@ const selectProductById = async (id) => {
   const [[product]] = await pool.query("SELECT * FROM products WHERE id = ?;", [
     id,
   ]);
-  return product;
+  const [seller] = await pool.query("SELECT * FROM users WHERE id = ?;", [product.sellerId]);
+  //console.log(result);
+  //const { id: productId, sellerId, ...productInfo } = result[0];
+  //const seller = { id: sellerId, ...result[0] };
+
+  return {
+    product: product,
+    seller:seller,
+  };
 };
 
 export default selectProductById;
